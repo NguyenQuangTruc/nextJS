d9d387ad055a53d639eb81c5d13f76e22b67c0ed
nguyen quang truc <nguyenquangtruc1452005@gmail.com>
2025-09-16 16:03:22 +0700

    test

---
+++ app/GioiThieu/page.tsx
@@ -0,0 +1,13 @@
+import Header from "../components/header"
+
+export default function GioiThieu() {
+    return (
+        <div>
+            <Header />
+            đây là trang giới thiệu
+            <div>
+                test
+            </div>
+        </div>
+    )
+}
